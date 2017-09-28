83c55b698897e8b9af94aad11310b9c0637b5624
Fitos Andor <andor.fitos@misys.com>
2017-09-28 21:51:15 +0200

    Initial Commit

---
+++ TransitiveExample/TransitiveExample.h
@@ -0,0 +1,19 @@
+//
+//  TransitiveExample.h
+//  TransitiveExample
+//
+//  Created by Andor Fitos on 2017. 09. 28..
+//  Copyright © 2017. AutSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TransitiveExample.
+FOUNDATION_EXPORT double TransitiveExampleVersionNumber;
+
+//! Project version string for TransitiveExample.
+FOUNDATION_EXPORT const unsigned char TransitiveExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TransitiveExample/PublicHeader.h>
+
+
